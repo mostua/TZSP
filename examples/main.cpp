65e7d7b0563290011beb38be73ce4bd8697557ee
mostua <mostua@Tophardware.pl>
2014-11-13 20:20:44 +0100

    decomposition in progress, created namespace reproduction

--- examples/main.cpp
+++ examples/main.cpp
@@ -5,9 +5,10 @@
 #include <set>
 #include "population.h"
 #include "square.h"
-
+#include "reproduction.h"
 
 using namespace std;
+
 
 template<unsigned int size>
 ostream& operator<<(ostream & os, const Square<size> &square)
@@ -86,50 +87,6 @@ int fitnessFunction(const Square<size>& square)
 	return result;
 }
 
-//reproduction function creates descendants
-template<unsigned int size>
-pair<Square<size>, Square<size>> reproductionFunction(const Square<size> & parentA, const Square<size> & parentB)
-{
-	//if (parentA.getSize() != parentB.getSize())
-	//	throw "Sheep can't reproduce with human";
-	//make one of children with parentA diagonals, and second with parentB diagonals
-	//copy child A from parent A and so for child B
-	Square<size> childA, childB;
-	childA = parentA, childB = parentB;
-	//swap (x,x) in childA to value of parentB (x,x) same to (n-x-1, x)
-	int x, y;
-	for (int i = 0; i < size; ++i)
-	{
-		x = childA.get(i, i);
-		y = parentB.get(i, i);
-		childA.swap(x, y);
-	}
-	for (int i = 0; i < size; ++i)
-	{
-		if (size % 2 == 1 && size / 2 == i)
-			continue;
-		x = childA.get(size - i - 1, i);
-		y = parentB.get(size - i - 1, i);
-		childA.swap(x, y);
-	}
-	for (int i = 0; i < size; ++i)
-	{
-		x = childB.get(i, i);
-		y = parentA.get(i, i);
-		childB.swap(x, y);
-	}
-	for (int i = 0; i < size; ++i)
-	{
-		if (size % 2 == 1 && size / 2 == i)
-			continue;
-		x = childB.get(size - i - 1, i);
-		y = parentA.get(size - i - 1, i);
-		childB.swap(x, y);
-	}
-	//return reproduces childs
-	pair<Square<size>, Square<size>> result = make_pair(childA, childB);
-	return result;
-}
 
 //Mutation function swaping two random points in 
 template<unsigned int size>
@@ -155,7 +112,7 @@ void perform(int reproducesNumber, Population<size> &population, Square<size> &b
     int i = 1;
 	do
 	{
-        population.generateNextPopulation(reproducesNumber, cmp, mutationFunction, fitnessFunction, reproductionFunction);
+        population.generateNextPopulation(reproducesNumber, cmp, mutationFunction, fitnessFunction, reproduction::reproductionFunction);
 		best = population.getBest(fitnessFunction);
         cout << "Iteration " << i++ << " best far now " << fitnessFunction(best) << endl;
 	} while (fitnessFunction(best) != 0);

fd3ecf4ffe09346a0df841ba8bc71c65b3408702
mostua <a.moscicki@stud.elka.pw.edu.pl>
2014-12-18 18:48:09 +0100

    refactoring tabs

---
+++ view/results.h
@@ -0,0 +1,18 @@
+#ifndef RESULTS_H
+#define RESULTS_H
+
+#include <QWidget>
+
+class Results : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit Results(QWidget *parent = 0);
+
+signals:
+
+public slots:
+
+};
+
+#endif // RESULTS_H
